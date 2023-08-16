697f9862bdf920accafb8d0404c7cc9d9548b7a7
toyobayashi <lifenglin314@outlook.com>
2023-08-16 11:31:33 +0800

    use `WASI_SDK_PATH/bin/clang`

--- build.sh
+++ build.sh
@@ -5,12 +5,14 @@ set -e
 wat2wasm -o ./src/fib.wasm ./src/fib.wat
 wat2wasm -o ./src/str.wasm ./src/str.wat
 
-clang -O3 -o ./src/fib_c.wasm \
+clang="$WASI_SDK_PATH/bin/clang"
+
+"$clang" -g -o ./src/fib_c.wasm \
       --target=wasm32-unknown-unknown \
       -nostdlib ./src/fib.c \
       -Wl,--import-undefined,--export-dynamic,--no-entry
 
-clang -O3 -o ./src/str_c.wasm \
+"$clang" -g -o ./src/str_c.wasm \
       --target=wasm32-unknown-unknown \
       -nostdlib ./src/str.c \
       -Wl,--import-undefined,--export-table,--export-memory,--export-dynamic,--no-entry
@@ -20,7 +22,7 @@ __dirname=$(cd `dirname $0`;pwd)
 node_include=$__dirname/node_modules/emnapi/include
 node_link=$__dirname/node_modules/emnapi/lib/wasm32
 
-clang -O3 -o ./src/demo_emnapi.wasm \
+"$clang" -g -o ./src/demo_emnapi.wasm \
       --target=wasm32-unknown-unknown \
       -I"$node_include" \
       -L"$node_link" \
@@ -30,7 +32,7 @@ clang -O3 -o ./src/demo_emnapi.wasm \
 
 node-gyp rebuild
 
-clang -O3 -o ./src/demo_pure.wasm \
+"$clang" -g -o ./src/demo_pure.wasm \
       --target=wasm32-unknown-unknown \
       -I"$node_include" \
       -nostdlib ./src/demo.c \
